b6696751f9d4d6effd163e7f2fc168c2256f7355
DhanunjayaMitta <102286105+DhanunjayaMitta@users.noreply.github.com>
2025-02-19 22:16:45 +0100

    Delete cypress/integration/examples/mousehover.js

--- cypress/integration/examples/mousehover.js
+++
@@ -1,9 +0,0 @@
-describe('mousehover', ()=>{
-    it('mhTestCase', () =>{
-        cy.visit(Cypress.env('url'))
-        //cy.get('.mouse-hover-content').invoke('show')
-        cy.contains('Top').click({force:true})
-        cy.url().should('include','top')
-    })
-
-})
